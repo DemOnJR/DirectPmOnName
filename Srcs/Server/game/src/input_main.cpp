8c3b3f7ac42a45ac4829219795c9b68acca373b5
Pipiasi Bogdan <pipiasibogdan@gmail.com>
2023-07-19 13:31:17 +0200

    Update input_main.cpp

--- Srcs/Server/game/src/input_main.cpp
+++ Srcs/Server/game/src/input_main.cpp
@@ -4,5 +4,5 @@ int len = snprintf(chatbuf, sizeof(chatbuf), "%s : %s", ch->GetName(), buf);
 // Change it like this
 int len = snprintf(chatbuf, sizeof(chatbuf), "|Hmsg:%s|h%s|h|r : %s", ch->GetName(), ch->GetName(), buf);
 
-// What you need to change is this:
+// To understand what i changed/added here it is:
 "|Hmsg:%s|h%s|h|r", ch->GetName(), ch->GetName()
